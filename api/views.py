bc7ab6c110816fb85ebfe417422f683e6a2dab7c
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-15 02:42:26 +0900

    viewsを定義。

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Task, Tag
+from rest_framework import viewsets
+from .serializers import TaskSerializer, TagSerializer
 
-# Create your views here.
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
